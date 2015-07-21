fb677c580f611946486229e9b5d5523bc65521f2
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-07-21 09:32:12 +0200

    updated logic functions, set and get

--- src/index.js
+++ src/index.js
@@ -16,27 +16,9 @@ function count(arr) {
  * @return {Array}
  */
 function and(arr1, arr2) {
-    var ans,
-        diff = arr1.length - arr2.length;
-    if (diff === 0) {
-        ans = new Array(arr1.length);
-        for (var i = 0; i < arr1.length; i++)
-            ans[i] = arr1[i] & arr2[i];
-    }
-    else if (diff < 0) {
-        ans = new Array(arr2.length);
-        for (var j = 0; j < diff; j++)
-            ans[j] = 0;
-        for (var k = 0; k < (arr2.length - diff); k++)
-            ans[k + diff] = arr1[k] & arr2[k + diff];
-    }
-    else {
-        ans = new Array(arr1.length);
-        for (var l = 0; l < diff; l++)
-            ans[l] = 0;
-        for (var m = 0; m < (arr1.length - diff); m++)
-            ans[m + diff] = arr1[m + diff] & arr2[m];
-    }
+    var ans = new Array(arr1.length);
+    for (var i = 0; i < arr1.length; i++)
+        ans[i] = arr1[i] & arr2[i];
     return ans;
 }
 
@@ -47,27 +29,9 @@ function and(arr1, arr2) {
  * @return {Array}
  */
 function or(arr1, arr2) {
-    var ans,
-        diff = arr1.length - arr2.length;
-    if (diff === 0) {
-        ans = new Array(arr1.length);
-        for (var i = 0; i < arr1.length; i++)
-            ans[i] = arr1[i] | arr2[i];
-    }
-    else if (diff < 0) {
-        ans = new Array(arr2.length);
-        for (var j = 0; j < diff; j++)
-            ans[j] = arr2[j];
-        for (var k = 0; k < (arr2.length - diff); k++)
-            ans[k + diff] = arr1[k] | arr2[k + diff];
-    }
-    else {
-        ans = new Array(arr1.length);
-        for (var l = 0; l < diff; l++)
-            ans[l] = arr1[l];
-        for (var m = 0; m < (arr1.length - diff); m++)
-            ans[m + diff] = arr1[m + diff] | arr2[m];
-    }
+    var ans = new Array(arr1.length);
+    for (var i = 0; i < arr1.length; i++)
+        ans[i] = arr1[i] | arr2[i];
     return ans;
 }
 
@@ -78,27 +42,9 @@ function or(arr1, arr2) {
  * @return {Array}
  */
 function xor(arr1, arr2) {
-    var ans,
-        diff = arr1.length - arr2.length;
-    if (diff === 0) {
-        ans = new Array(arr1.length);
-        for (var i = 0; i < arr1.length; i++)
-            ans[i] = (arr1[i] | arr2[i]) & ~(arr1[i] & arr2[i]);
-    }
-    else if (diff < 0) {
-        ans = new Array(arr2.length);
-        for (var j = 0; j < diff; j++)
-            ans[j] = arr2[j];
-        for (var k = 0; k < (arr2.length - diff); k++)
-            ans[k + diff] = (arr1[k] | arr2[k + diff]) & ~(arr1[k] & arr2[k + diff]);
-    }
-    else {
-        ans = new Array(arr1.length);
-        for (var l = 0; l < diff; l++)
-            ans[l] = arr1[l];
-        for (var m = 0; m < (arr1.length - diff); m++)
-            ans[m + diff] = (arr1[m + diff] | arr2[m]) & ~(arr1[m + diff] & arr2[m]);
-    }
+    var ans = new Array(arr1.length);
+    for (var i = 0; i < arr1.length; i++)
+        ans[i] = (arr1[i] | arr2[i]) & ~(arr1[i] & arr2[i]);
     return ans;
 }
 
@@ -108,37 +54,41 @@ function xor(arr1, arr2) {
  * @return {Array}
  */
 function not(arr) {
-    var ans  =  new Array(arr.length);
+    var ans = new Array(arr.length);
     for (var i = 0; i < ans.length; i++)
         ans[i] = ~arr[i];
     return ans;
 }
 
 /**
- * Gets the n value
- * @param {number} orig
+ * Gets the n value of array arr
+ * @param {Array} arr
  * @param {number} n
- * @return {number}
+ * @return {boolean}
  */
-function getBit(orig, n) {
-    if ((Math.pow(2, n) & orig) === 0)
-        return 0;
-    return 1;
+function getBit(arr, n) {
+    var index = n >> 5; // Same as Math.floor(n/32)
+    var mask = 1 << (31 - n % 32);
+    return Boolean(arr[index] & mask);
 }
 
 /**
- * Sets the n value to the value val
- * @param {number} orig
+ * Sets the n value of array arr to the value val
+ * @param {Array} arr
  * @param {number} n
- * @param {number} val
- * @return {number}
+ * @param {boolean} val
+ * @return {Array}
  */
-function setBit(orig, n, val) {
-    if (val !== 1 && val !== 0)
-        throw new RangeError('val should be 1 or 0');
-    if (n < 0 || n > (Math.floor(Math.log(orig) / Math.log(2)) + 1))
-        throw new RangeError('n out of bounds');
-    return ((Math.pow(2, n) * val) | (~Math.pow(2, n) & orig));
+function setBit(arr, n, val) {
+    var index = n >> 5; // Same as Math.floor(n/32)
+    var mask = 1 << (31 - n % 32);
+    var ans = arr.concat();
+    if (val)
+        ans[index] = mask | (~mask & ans[index]);
+    else
+        ans[index] = ~mask & ans[index];
+    return ans;
+   // ((Math.pow(2, n) * val) | (~Math.pow(2, n) & arr[0]));
 }
 
 module.exports = {
---
+++ test/test.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var bitArray = require('..');
+
+var t1 = [0,1,0,1];
+var t2 = [0,0,1,1];
+
+describe('Bit array test', function () {
+
+    it('AND test', function () {
+        var and = bitArray.and(t1, t2);
+        and[0].should.equal(0);
+        and[1].should.equal(0);
+        and[2].should.equal(0);
+        and[3].should.equal(1);
+    });
+
+    it('OR test', function () {
+        var or = bitArray.or(t1, t2);
+        or[0].should.equal(0);
+        or[1].should.equal(1);
+        or[2].should.equal(1);
+        or[3].should.equal(1);
+    });
+
+    it('XOR test', function () {
+        var xor = bitArray.xor(t1, t2);
+        xor[0].should.equal(0);
+        xor[1].should.equal(1);
+        xor[2].should.equal(1);
+        xor[3].should.equal(0);
+    });
+
+    it('NOT test', function () {
+        var not = bitArray.not(t2);
+        not[0].should.equal(-1);
+        not[1].should.equal(-1);
+        not[2].should.equal(-2);
+        not[3].should.equal(-2);
+    });
+
+    it('get test', function () {
+        bitArray.getBit(t1, 2).should.be.false();
+        bitArray.getBit(t1, 32).should.be.false();
+        bitArray.getBit(t1, 63).should.be.true();
+    });
+
+    it('set test', function () {
+        bitArray.getBit(t1, 2).should.be.false();
+        var ans = bitArray.setBit(t1, 2, false);
+        bitArray.getBit(ans, 2).should.be.false();
+        ans = bitArray.setBit(t1, 2, true);
+        bitArray.getBit(ans, 2).should.be.true();
+    });
+});
